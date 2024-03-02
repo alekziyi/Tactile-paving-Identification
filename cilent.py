20316e5f11050697f0cdfba430b5378ad57a73fc
alekziyi <alek_ziyi@outlook.com>
2024-03-02 22:33:54 +0800

    debug

--- cilent.py
+++ cilent.py
@@ -4,25 +4,25 @@ import numpy as np
 import time
 from threading import Thread
 
-PACKAGE_SIZE=65000
+PACKAGE_SIZE=61440
 
 class VideoReporter(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect(('localhost', 12000))
-        self.cap = cv2.VideoCapture(0)
+        self.client_socket.connect(('192.168.137.1', 12000))
+        self.cap = cv2.VideoCapture(1)
         self.num=0
         self.center=None
     def send(self,img: np.ndarray):
-        n=img.size//PACKAGE_SIZE
-        self.client_socket.send(n.to_bytes(4))
-        self.client_socket.recv(1)
-        for i in range(n):
-            self.client_socket.send(img.data[i*PACKAGE_SIZE:(i+1)*PACKAGE_SIZE])
-            self.client_socket.recv(1)
-        self.client_socket.send(img.data[n * PACKAGE_SIZE:])
-        self.client_socket.recv(1)
+        for i in range(15):
+            self.send_package(img.data[i*PACKAGE_SIZE:(i+1)*PACKAGE_SIZE])
+    def send_package(self,data):
+        self.client_socket.sendall(data)
+        re=int.from_bytes(self.client_socket.recv(4), byteorder='little')
+        while re != 1:
+            self.client_socket.sendall(data)
+
     def receive(self):
         # self.center=eval(self.client_socket.recv(50).decode('utf-8'))
         # self.client_socket.send(b'1')
--- server.py
+++ server.py
@@ -5,33 +5,32 @@ import numpy as np
 from threading import Thread
 from deeplab import DeeplabV3
 
-PACKAGE_SIZE=65000
+PACKAGE_SIZE=61440
 
 class VideoProcessor(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.deeplab = DeeplabV3(num_classes=2)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(('localhost', 12000))
+        self.sock.bind(('192.168.137.1', 12000))
         self.sock.listen(1)
         self.client_socket, self.client_address = self.sock.accept()
 
 
     def receive_image(self):
-        n=int.from_bytes(self.client_socket.recv(4))
-        self.client_socket.send(b'1')
         data=b''
-        for i in range(n):
-            data+=self.client_socket.recv(PACKAGE_SIZE)
-            self.client_socket.send(b'1')
-        data += self.client_socket.recv(PACKAGE_SIZE)
-        self.client_socket.send(b'1')
-        print(len(data))
-        if len(data) != 921600:
-            return np.zeros((480,640,3),dtype=np.uint8)
+        for i in range(15):
+            print(i)
+            data +=self.receive_package()
         img=np.frombuffer(data, dtype=np.uint8).reshape(480,640,3)
         return img
-
+    def receive_package(self):
+        data=self.client_socket.recv(PACKAGE_SIZE)
+        while len(data)!=PACKAGE_SIZE:
+            self.client_socket.sendall((0).to_bytes(4,byteorder='little'))
+            data=self.client_socket.recv(PACKAGE_SIZE)
+        self.client_socket.sendall((1).to_bytes(4,byteorder='little'))
+        return data
     def image_processing(self, img):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         binary = self.deeplab.detect_image(img)
@@ -40,7 +39,7 @@ class VideoProcessor(Thread):
     def get_img_center(self,binary):
         return (0,0)
     def send(self,center):
-        # self.client_socket.send(f'{center}'.encode('utf-8'))
+        #self.client_socket.sendall(f'{center}'.encode('utf-8'))
         # self.client_socket.recv(1)
         pass
     def run(self):
@@ -48,9 +47,11 @@ class VideoProcessor(Thread):
             img=self.receive_image()
             cv2.imshow('img',img)
             cv2.waitKey(1)
+            #print(img)
             binary=self.image_processing(img)
             cv2.imshow('binary',binary)
             cv2.waitKey(1)
+            #print(binary)
             center=self.get_img_center(binary)
             self.send(center)
 
